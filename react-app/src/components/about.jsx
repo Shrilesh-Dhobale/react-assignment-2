8d75f439a5e4365a5643fedf1a90d7470a66ba7d
MaithilyKawale <maithilyworkspace@gmail.com>
2025-10-28 21:07:59 +0530

    more information added in pages

--- react-app/src/components/about.jsx
+++ react-app/src/components/about.jsx
@@ -1,7 +1,59 @@
-function About(){
-    return(
-        <p>This is about page.</p>
-    )
+
+function About() {
+  return (
+    <>
+      <section>
+        <h2>ℹ️Introduction</h2>
+        <ul>
+          <li>
+            My name is <strong>Maithily Subhash Kawale</strong> from{" "}
+            <em><u>Lakhani</u></em>.
+          </li>
+          <li>I am <strong>21 years old</strong> and have a <u>strong academic background</u>.</li>
+          <li>I am currently studying at <strong><em>GH Raisoni College of Engineering (GHRCE), Nagpur</em></strong>.</li>
+          <li>
+            I completed my 10<sup>th</sup> grade from <strong><em>UEMS</em></strong> with an impressive score of{" "}
+            <u><em>93%</em></u>.
+          </li>
+          <li>
+            After that, I pursued my 12<sup>th</sup> education at <strong><em>Samarth Mahavidyalay</em></strong>, where I secured{" "}
+            <u><em>73%</em></u>.
+          </li>
+        </ul>
+      </section>
+
+      <hr />
+
+      <section>
+        <h2>
+          🙋‍♀️About Me
+        </h2>
+        <ul>
+          <li>
+            <strong>Hobbies:</strong> Travelling, exploring new places and things, dancing, <del>watching TV</del>, reading books.
+          </li>
+          <li><strong>Strengths:</strong> Quick learner, punctual, responsible.</li>
+          <li><strong>Weaknesses:</strong> Overthinking, impatience, short tempered.</li>
+        </ul>
+      </section>
+
+      <hr />
+
+      <section>
+        <h2>
+          💭My thoughts
+        </h2>
+        <ul>
+          <li>
+            I have a keen interest in <em>learning new technologies</em>, and currently I am focusing on <u>learning HTML to build my skills in web development</u>.
+          </li>
+          <li>
+            I believe in <u>continuous learning</u> and always try to give my best in whatever I do. This project reflects my understanding and effort in HTML and I hope it adds value to my learning journey.
+          </li>
+        </ul>
+      </section>
+    </>
+  );
 }
 
 export default About;
--- react-app/src/components/home.jsx
+++ react-app/src/components/home.jsx
@@ -1,8 +1,15 @@
-function Home(){
-    return(
-        <p>This is home page.</p>
-    )
-    
+
+function Home() {
+    return (
+        <>
+            <div>
+                <h2>Hello World!</h2>
+            </div>
+            <div>
+                <p>Hi, I’m Maithily Kawale, a student passionate about learning and growing in tech.</p>
+            </div>
+        </>
+    );
 }
 
 export default Home;
